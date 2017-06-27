456fe910b2f84aa931b934d9188e7479c40cc0b7
hel <tmp@sth.nowhere>
2017-06-27 11:05:08 +0200

    organised structure

--- TestFile.py
+++ TestFile.py
@@ -1,22 +1,11 @@
-import pandas as pd
 import numpy as np
-from scipy import integrate
+import pandas as pd
 import matplotlib.pyplot as plt
+from scipy import stats
+import os
+import datetime
+import random
+from scipy.stats.stats import pearsonr
 
-rng = pd.date_range('1/1/2011', periods=360, freq='1 min')
-dat = pd.DataFrame(np.random.rand(len(rng)))
-dat['time'] = rng
-dat = dat.set_index(pd.to_datetime(dat['time']))
-del dat['time']
-dat.columns = ['values']
-
-# now check how to integrate the data
-
-df_int = integrate.cumtrapz(dat['values'], dat.index.astype(np.int64) / 10**9, initial=0)
-
-dat = pd.DataFrame(np.empty(len(rng)))
-dat['time'] = rng
-dat['integral'] = df_int
-dat.plot(x='time', y='integral')
-plt.show()
-#dat['integral'] = df_int
+s = pd.Series([1, 2, 3, 4, np.nan, np.nan, np.nan])
+print s.isnull().sum()
